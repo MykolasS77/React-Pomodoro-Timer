0645f9924a0d62245a6282aa3de3a2067709a017
Mykolas <m.saug77@gmail.com>
2025-01-07 22:30:42 +0200

    pomadoro timer

---
+++ src/app.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import FormTemplate from "./form"
+
+function App() {
+
+      return (
+        <div>
+        <FormTemplate />
+        </div> 
+      );
+ 
+}
+ 
+export default App
---
+++ src/form.jsx
@@ -0,0 +1,134 @@
+import React, { useState, useEffect } from 'react';
+
+function FormTemplate(props){
+
+    const [secconds, setSecconds] = useState(0)
+    const [minutes, setMinutes] = useState(0)
+    const [hours, setHours] = useState(0)
+    const [startTimer, updateStart] = useState(false)
+    const intervalRef = React.useRef(null);
+    
+    const time_2 = new Date()
+    time_2.setHours(hours)
+    time_2.setMinutes(minutes)
+    time_2.setSeconds(secconds)
+    
+
+    function set_timer(event){
+
+        let action_name = event.target.name
+
+        switch(action_name){
+            case "+secconds":
+                var new_number = secconds + 1
+                setSecconds(new_number)
+                break
+            case "-secconds":
+                var new_number = secconds - 1
+                setSecconds(new_number)
+                break
+            case "+minutes":
+                var new_number = minutes + 1
+                setMinutes(new_number)
+                break
+            case "-minutes":
+                var new_number = minutes - 1
+                setMinutes(new_number)
+                break
+            case "+hours":
+                var new_number = hours + 1
+                setHours(new_number)
+                break
+            case "-hours":
+                var new_number = hours - 1
+                setHours(new_number)
+                break
+        }
+    }
+
+    useEffect(() => {
+        return () => stopCounter(); 
+      }, []);
+
+    const startCounter = (event) => {
+    if (intervalRef.current) return;
+    intervalRef.current = setInterval(() => {
+        console.log(event.target.name)
+        let action_name = event.target.name
+        switch(action_name){
+            case "+secconds":
+                setSecconds((prev_number) => prev_number + 1)
+                break
+            case "-secconds":
+                setSecconds((prev_number) => prev_number - 1)
+                break
+            case "+minutes":
+                var new_number = minutes + 1
+                setMinutes((prev_number) => prev_number + 1)
+                break
+            case "-minutes":
+                var new_number = minutes - 1
+                setMinutes((prev_number) => prev_number - 1)
+                break
+            case "+hours":
+                var new_number = hours + 1
+                setHours((prev_number) => prev_number + 1)
+                break
+            case "-hours":
+                var new_number = hours - 1
+                setHours((prev_number) => prev_number - 1)
+                break
+        }
+    }, 150);
+    };
+
+    const stopCounter = () => {
+        if (intervalRef.current) {
+          clearInterval(intervalRef.current);
+          intervalRef.current = null;
+        }
+      };
+
+    function refresh_pomadoro(){
+        
+        setSecconds((prevState)=> {
+            var newDate = prevState - 1
+            console.log(prevState)
+            return newDate
+        })
+    
+        }
+    
+        function start_timer(){
+        updateStart(true)
+        }
+        function stop_timer(){
+            updateStart(false)
+            }
+    
+        useEffect(() => {
+        if (startTimer == true) {
+    
+            const interval = setInterval(refresh_pomadoro, 1000);
+        
+            return () => clearInterval(interval);
+        }
+        }, [startTimer]);
+    
+    return(
+        <div>
+            <div>
+                <h1>{time_2.toLocaleTimeString("uk-Uk")}</h1>
+                <p>Adjust secconds <button name="+secconds" onClick={set_timer} onMouseDown={startCounter} onMouseUp={stopCounter}>+</button> <button name="-secconds" onClick={set_timer} onMouseDown={startCounter} onMouseUp={stopCounter}>-</button> </p>
+                <p>Adjust minutes <button name="+minutes" onClick={set_timer} onMouseDown={startCounter} onMouseUp={stopCounter}>+</button> <button name="-minutes" onClick={set_timer} onMouseDown={startCounter} onMouseUp={stopCounter}>-</button> </p>
+                <p>Adjust hours <button name="+hours" onClick={set_timer} onMouseDown={startCounter} onMouseUp={stopCounter}>+</button> <button name="-hours" onClick={set_timer} onMouseDown={startCounter} onMouseUp={stopCounter}>-</button> </p>
+                <button onClick={start_timer} >Start Timer</button>
+                <button onClick={stop_timer}>Stop Timer</button>
+            </div>
+        </div>
+        
+    )
+}
+
+export default FormTemplate
+
